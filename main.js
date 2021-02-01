5a3ebd354482b75e6f7c6b1a08289d3e0ffcd02c
pythonoideas W10 tiny <pythonoideas@gmail.com>
2021-02-01 13:15:43 -0600

    Iniciando el repositorio en Windows como editor

---
+++ main.js
@@ -0,0 +1,39 @@
+var edad = 18;
+var  limVotar = 18;
+
+if (edad>= limVotar) {
+    console.log('Puede votar');
+} else {
+  console.log('No puede votar');
+}
+
+var edad = 18;
+var  limVotar = 18;
+var ciudad= "cdmx";
+
+if (edad>= limVotar && ciudad === 'CDMX') {
+    console.log('Puede votar');
+} else {
+  console.log('No puede votar');
+}
+
+switch(ciudad) {
+    case 'CDMX':
+        console.log('Azcapotzalco, Benito Juarez...');
+    break;
+    case 'Pachuca':
+        console.log('Mineral, Periodistas...');
+    break;
+    case 'Tulancingo':
+        cinsole.log('Centro, la Morena...');
+    default:
+        console.log('No encontre lo que buscas');
+        break;
+}
+for(var i=1; i <=10; i+4) {
+    console.log('i vale ', i);
+    }
+
+    for(var i=1; i <=10; i=i+3) {
+    console.log('i vale', i);
+    }
