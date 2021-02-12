e6a6e854465049e7deb878cd5552be76174bbd95
pythonoideas W10 tiny <pythonoideas@gmail.com>
2021-02-12 14:52:41 -0600

    algunos cambios para probar la clase 1

--- main.js
+++ main.js
@@ -1,39 +1,48 @@
-var edad = 18;
-var  limVotar = 18;
+// var edad = 18;
+// var  limVotar = 18;
 
-if (edad>= limVotar) {
-    console.log('Puede votar');
-} else {
-  console.log('No puede votar');
-}
+// if (edad>= limVotar) {
+//     console.log('Puede votar');
+// } else {
+//   console.log('No puede votar');
+// }
 
-var edad = 18;
-var  limVotar = 18;
-var ciudad= "cdmx";
+// var edad = 18;
+// var  limVotar = 18;
+// var ciudad= "cdmx";
 
-if (edad>= limVotar && ciudad === 'CDMX') {
-    console.log('Puede votar');
-} else {
-  console.log('No puede votar');
-}
+// if (edad>= limVotar && ciudad === 'CDMX') {
+//     console.log('Puede votar');
+// } else {
+//   console.log('No puede votar');
+// }
 
-switch(ciudad) {
-    case 'CDMX':
-        console.log('Azcapotzalco, Benito Juarez...');
-    break;
-    case 'Pachuca':
-        console.log('Mineral, Periodistas...');
-    break;
-    case 'Tulancingo':
-        cinsole.log('Centro, la Morena...');
-    default:
-        console.log('No encontre lo que buscas');
-        break;
-}
-for(var i=1; i <=10; i+4) {
-    console.log('i vale ', i);
-    }
+// switch(ciudad) {
+//     case 'CDMX':
+//         console.log('Azcapotzalco, Benito Juarez...');
+//     break;
+//     case 'Pachuca':
+//         console.log('Mineral, Periodistas...');
+//     break;
+//     case 'Tulancingo':
+//         cinsole.log('Centro, la Morena...');
+//     default:
+//         console.log('No encontre lo que buscas');
+//         break;
+// }
+// for(var i=1; i <=10; i+4) {
+//     console.log('i vale ', i);
+//     }
 
-    for(var i=1; i <=10; i=i+3) {
-    console.log('i vale', i);
-    }
+//     for(var i=1; i <=10; i=i+3) {
+//     console.log('i vale', i);
+//     }
+var a = 5
+var b = 10
+var c = 15
+console.log (( 3 + b ) * c / a + 2)     //  41
+console.log ((( a + b * c) / 5) * 2)    //  62
+console.log (b / a + 2 * c)             //  32
+console.log ((a + b + c / c) + a)       //  21
+console.log (((3 + a) / c) + a + b + c) //  30.53
+console.log (a - (b + a) * a / 1)       //  -70
